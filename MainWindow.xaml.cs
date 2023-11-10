c6062e742370c6bf75e6b5fb6c460dfb54ce578e
Nguyen Viet Tu <tunv@ZOD.VN>
2023-11-11 00:48:08 +0700

    Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GrapeCity.ActiveReports.Samples.WPFViewer
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		static readonly string CurrentFileLocation = AppDomain.CurrentDomain.BaseDirectory + @"..\..\..\Catalog.rdlx";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+            ReportViewer.LoadDocument(CurrentFileLocation);
+        }
+	}
+}
